3bca8b6907c4587d7fe20ef8b6864d3e7fb47140
farulwananda <farulahmad2002@gmail.com>
2022-03-27 19:01:31 +0700

    remake: nextra

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/farulwananda',
+  docsRepositoryBase: 'https://github.com/farulwananda/fawdocs',
+  titleSuffix: ' – FDocs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">FDocs</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Farul Wananda's Docs
       </span>
     </>
   ),
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Farul Wananda.</>,
   unstable_faviconGlyph: '👋',
 }
